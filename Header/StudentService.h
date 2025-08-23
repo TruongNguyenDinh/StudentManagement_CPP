3200de29d59d68e8b119f04460d89b40ac181449
Nguyen Dinh Truong <truongnd1124@gmail.com>
2025-08-23 16:46:46 +0700

    Create Header folder and Refactor

---
+++ Header/StudentService.h
@@ -0,0 +1,22 @@
+#ifndef STUDENT_SERVICE_H
+#define STUDENT_SERVICE_H
+
+#include"Student.h"
+#include<vector>
+using namespace std;
+class StudentService{
+    private:
+    vector<Student> students;
+    bool isInteger(const string& s);
+    static int autoGID;
+    public:
+    void addStudent(const Student& s);
+    bool removeStudent(int id);
+    void displayAll() const;
+    Student* findStudent(const string& s);
+    void editName(int id,const string& newName);
+    void editAge(int id,int newAge);
+    void editScore(int id,double newScore);
+
+};
+#endif
